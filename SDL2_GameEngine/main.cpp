74ed4e5e496a0080dd488bc34caeaf49c5ed182b
hsetlik <haydensetlik@gmail.com>
2020-08-14 11:42:18 -0600

    Initial Commit

---
+++ SDL2_GameEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL2_GameEngine
+//
+//  Created by Hayden on 8/14/20.
+//  Copyright © 2020 Hayden Setlik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
